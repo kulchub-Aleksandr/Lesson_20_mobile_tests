d8370ec8a7a8ab2c574fc52acaf040e8151281f8
Aleksandr <kulchub@gmail.com>
2026-04-24 19:28:13 +0500

    add test

---
+++ src/test/java/tests/local/SearchTests.java
@@ -0,0 +1,27 @@
+package tests.local;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests extends TestBase {
+
+    @Test
+    void successfulSearchTest() {
+        back();
+        step("Type search", () -> {
+            $(id("org.wikipedia.alpha:id/search_text_view")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () ->
+            $$(id("org.wikipedia.alpha:id/fragment_search_results"))
+                    .shouldHave(sizeGreaterThan(0)));
+    }
+}
+//org.wikipedia.alpha:id/search_text_view
+//org.wikipedia.alpha:id/search_src_text
+//org.wikipedia.alpha:id/search_text_view
+//org.wikipedia.alpha:id/fragment_search_results
