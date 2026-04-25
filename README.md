9426e562f595e6904af935aa99e7f021f0cae8c5
Aleksandr <kulchub@gmail.com>
2026-04-25 22:20:39 +0500

    fix code

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Запуск локально (local) на эмуляторе:
 ```bash 
-gradle clean local_test -DdeviceHost=local
+ ./gradle clean local_test -DdeviceHost=local
 ```
 
 Запуск удаленно (remote) на Browserstack:
 ```bash 
-gradle clean remote_test -DdeviceHost=remote
+ ./gradle clean remote_test -DdeviceHost=remote
 ```
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,21 +18,29 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestBase {
     public static String deviceHost = System.getProperty("deviceHost");
 
+//    private static final String BROWSERSTACK_DRIVER = "remote",
+//            LOCAL_DRIVER = "local";
+
     @BeforeAll
     static void beforeAll() {
 
-            if (deviceHost == null) {
-                deviceHost = "remote";
+//            switch (System.getProperty("deviceHost", BROWSERSTACK_DRIVER)) {
+//                case BROWSERSTACK_DRIVER -> Configuration.browser = BrowserStackDriver.class.getName();
+//                case LOCAL_DRIVER -> Configuration.browser = LocalDriver.class.getName();
+//            }
 
-            }
-            switch (deviceHost) {
-                case "remote":
-                    Configuration.browser = BrowserStackDriver.class.getName();
-                    break;
-                case "local":
-                    Configuration.browser = LocalDriver.class.getName();
-                    break;
-            }
+
+        deviceHost = System.getProperty("deviceHost", "local"); // default local
+
+        switch (deviceHost.toLowerCase()) {
+            case "remote":
+                Configuration.browser = BrowserStackDriver.class.getName();
+                break;
+            case "local":
+            default:
+                Configuration.browser = LocalDriver.class.getName();
+                break;
+        }
         Configuration.browserSize = null;
         Configuration.timeout = 30000;
     }
